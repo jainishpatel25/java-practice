88a3c6de24c11d9a99465d370b12db332d1cd11c
jainishpatel25 <jainish637@gmail.com>
2026-01-10 00:58:35 +0530

    Multithreading Example

--- src/ThreadExample/ThreadClass/MainAndChildThread.java
+++ src/ThreadExample/ThreadClass/MainAndChildThread.java
@@ -2,17 +2,25 @@ package ThreadExample.ThreadClass;
 
 class MyThread extends Thread {
     public void run() {
-        for(int i = 1; i <= 5; i++) {
+        for(int i = 1; i <= 3; i++) {
             System.out.println("Child Thread: " + i);
+//            Thread.yield();
+//            try {
+//                Thread.sleep(4000);                  // currently running thread pause for a specified time.
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         MyThread t = new MyThread();
         t.start();
 
+//        t.join();     //let finish to other thread first
 
-        for(int i = 1; i <= 5; i++) {
+
+        for(int i = 1; i <= 3; i++) {
             System.out.println("Main Thread: " + i);
         }
     }
--- src/ThreadExample/ThreadClass/SimpleThread.java
+++ src/ThreadExample/ThreadClass/SimpleThread.java
@@ -3,11 +3,17 @@ package ThreadExample.ThreadClass;
  class SimpleThread extends Thread{
 
     public void run() {
+        System.out.println("Priority: " + getPriority());
         System.out.println("Thread is running");
     }
 
     public static void main(String[] args) {
         SimpleThread t = new SimpleThread();
+
+//        System.out.println(t.getName());
+//        t.setName("Main-Thread");
+//        System.out.println(t.getName());
+        t.setPriority(10);
         t.start();   // starts new thread
 
     }
